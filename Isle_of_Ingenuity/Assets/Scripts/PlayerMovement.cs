808bb1a914eea000ff4456afd99dc463882d093b
zgurley1 <zgurley1@charlotte.edu>
2025-03-15 00:35:46 -0400

    terrain updates

--- Isle_of_Ingenuity/Assets/Scripts/PlayerMovement.cs
+++ Isle_of_Ingenuity/Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
+    public float mouseSensitivity = 2f;
+    
     private bool isGrounded;
-
     private Rigidbody rb;
+    private float rotationX = 0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked; // Lock cursor to center of screen
     }
 
     void Update()
     {
+        // Player movement
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(moveX, 0, moveZ) * moveSpeed;
-        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
-
-
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        rb.linearVelocity = new Vector3(move.x * moveSpeed, rb.linearVelocity.y, move.z * moveSpeed);
         
+        // Jumping
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
+
+        // Mouse look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f); // Limit vertical rotation
         
+        transform.Rotate(Vector3.up * mouseX);
+        Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -38,14 +49,6 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
-        }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
         }
     }
 }
