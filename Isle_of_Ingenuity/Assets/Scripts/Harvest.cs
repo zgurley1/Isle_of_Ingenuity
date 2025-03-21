3ba7c4a4d9b89e43daa9353bd034ad7458ddf751
Cjones0985 <cjones1480985@gmail.com>
2025-03-21 15:23:29 -0400

    Added stone harvesting and pickaxe Item

--- Isle_of_Ingenuity/Assets/Scripts/Harvest.cs
+++ Isle_of_Ingenuity/Assets/Scripts/Harvest.cs
@@ -5,14 +5,17 @@ public class Harvest : MonoBehaviour
 
     public Item logItem;
     public Item axeItem;
-    private bool isPlayerInRange = false;
+    public Item pickaxeItem;
+    public Item stoneItem;
+    private bool isPlayerInTreeRange = false;
+    private bool isPlayerInRockRange = false;
 
     void Start() {
         
     }
     
     void Update() {
-        if (isPlayerInRange && Input.GetMouseButtonDown(0)) {
+        if (isPlayerInTreeRange && Input.GetMouseButtonDown(0)) {
             if (InventoryManager.instance.GetSelectedItem(true) == axeItem) {
                 if (logItem != null && InventoryManager.instance != null) {
                 InventoryManager.instance.AddItem(logItem);
@@ -23,19 +26,41 @@ public class Harvest : MonoBehaviour
             }
             
         }
+
+        if (isPlayerInRockRange && Input.GetMouseButtonDown(0)) {
+            if (InventoryManager.instance.GetSelectedItem(true) == pickaxeItem) {
+                if (stoneItem != null && InventoryManager.instance != null) {
+                InventoryManager.instance.AddItem(stoneItem);
+                Debug.Log("Collected Stone!");
+                } else {
+                    Debug.LogError("Rock item or InventoryManager is NULL.");
+                }
+            }
+            
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Tree")) {
-            isPlayerInRange = true;
+            isPlayerInTreeRange = true;
             Debug.Log("Player entered tree range.");
+        }
+
+        if (other.CompareTag("Rock")) {
+            isPlayerInRockRange = true;
+            Debug.Log("Player entered rock range.");
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.CompareTag("Tree")) {
-            isPlayerInRange = false;
+            isPlayerInTreeRange = false;
             Debug.Log("Player exited tree range.");
+        }
+
+        if (other.CompareTag("Rock")) {
+            isPlayerInRockRange = false;
+            Debug.Log("Player exited rock range.");
         }
     }
 
