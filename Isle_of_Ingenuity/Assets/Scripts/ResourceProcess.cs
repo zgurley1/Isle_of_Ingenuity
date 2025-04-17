f51560cfe645c84ef29ac5fee3acacb506475d8c
zgurley1 <zgurley1@charlotte.edu>
2025-04-17 19:49:02 -0400

    Find elements in inv.

--- Isle_of_Ingenuity/Assets/Scripts/ResourceProcess.cs
+++ Isle_of_Ingenuity/Assets/Scripts/ResourceProcess.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ResourceProcess : MonoBehaviour
@@ -6,9 +8,18 @@ public class ResourceProcess : MonoBehaviour
     
     private bool seek = true;
     private InventoryController InventoryController;
+
+    public List<int> woodSlots = new List<int>();
+    public List<int> stoneSlots = new List<int>();
+
+    private InventoryManager InventoryManager;
+
+    [SerializeField] private Item wood;
+    [SerializeField] private Item stone;
     void Start()
     {
         InventoryController = FindAnyObjectByType<InventoryController>();
+        InventoryManager = FindAnyObjectByType<InventoryManager>();
     }
 
     // Update is called once per frame
@@ -29,8 +40,34 @@ public class ResourceProcess : MonoBehaviour
                 if (Input.GetMouseButtonDown(0)) // Left-click
                 {
                     InventoryController.OpenProcessUI();
+                    ClearList();
+                    FillList();
+
+                    Debug.Log("Wood slots length" + woodSlots.Count);
+                    Debug.Log("Stone slots length" + stoneSlots.Count);
+
                 }
             }
         }
     }
+
+    void ClearList() {
+        woodSlots.Clear();
+        stoneSlots.Clear();
+    }
+    void FillList() {
+        for (int i = 0; i < InventoryManager.inventorySlots.Length; i++) {
+            InventorySlot slot = InventoryManager.inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot != null && itemInSlot.item == wood) {
+                woodSlots.Add(i);
+                
+            } else if (itemInSlot != null && itemInSlot.item == stone) {
+                stoneSlots.Add(i);
+                
+            }
+        }
+    }
+
+
 }
