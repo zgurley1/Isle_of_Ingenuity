a533b475c54ab622230687b1a3ed93695a0fd807
Zachary Gurley <zgurley1@charlotte.edu>
2025-04-17 15:05:27 -0400

    Boat Upgrade inital implementation

--- Isle_of_Ingenuity/Assets/Scripts/UpgradeActions.cs
+++ Isle_of_Ingenuity/Assets/Scripts/UpgradeActions.cs
@@ -5,17 +5,23 @@ public class UpgradeActions : MonoBehaviour
 {
     public GameObject fixedDockPrefab;
     public GameObject brokenDockInScene;
+
+    public GameObject boatPosition;
+    public GameObject boat;
     public Button fixDockButton;
+
+    public Button buildBoatButton;
 
     public void FixDock()
     {
         if (brokenDockInScene != null && fixedDockPrefab != null)
         {
             Vector3 dockPosition = brokenDockInScene.transform.position;
+            Vector3 dockPositionOffset = new Vector3(dockPosition.x, dockPosition.y - 2, dockPosition.z);
             Quaternion dockRotation = brokenDockInScene.transform.rotation;
 
             Destroy(brokenDockInScene);
-            Instantiate(fixedDockPrefab, dockPosition, dockRotation);
+            Instantiate(fixedDockPrefab, dockPositionOffset, dockRotation);
 
             if (fixDockButton != null)
             {
@@ -27,6 +33,29 @@ public class UpgradeActions : MonoBehaviour
         else
         {
             Debug.LogWarning("Dock references not set in UpgradeActions script.");
+        }
+    }
+
+    public void BuildBoat()
+    {
+        if (boatPosition != null && boat != null)
+        {
+            Vector3 boatPlacePosition = boatPosition.transform.position;
+            Quaternion boatPlaceRotation = boatPosition.transform.rotation;
+
+            Destroy(boatPosition);
+            Instantiate(boat, boatPlacePosition, boatPlaceRotation);
+
+            if (buildBoatButton != null)
+            {
+                buildBoatButton.interactable = false;
+            }
+
+            Debug.Log("Boat Built!");
+        }
+        else
+        {
+            Debug.LogWarning("Boat references not set in UpgradeActions script.");
         }
     }
 }
