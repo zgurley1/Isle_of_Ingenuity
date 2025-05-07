2b2f7471235fb312000f1e5a4aba77f9013d8cd2
Cjones0985 <cjones1480985@gmail.com>
2025-05-07 16:53:54 -0400

    Spinner when harvesting stuffs

--- Isle_of_Ingenuity/Assets/Scripts/Harvest.cs
+++ Isle_of_Ingenuity/Assets/Scripts/Harvest.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
+using Assets.SimpleSpinner;
 
 public class Harvest : MonoBehaviour
 {
     private Harvestable currentTarget;
+    public SimpleSpinner spinner;
+
+    void Start() {
+        if (spinner == null) {
+            spinner = GetComponentInChildren<SimpleSpinner>();
+            if (spinner == null) {
+                Debug.LogWarning("Spinner not assigned or found in children!");
+            }
+        }
+    }
 
     void Update() {
         if (currentTarget != null && Input.GetMouseButtonDown(0)) {
             Item selectedTool = InventoryManager.instance.GetSelectedItem(true);
+            // spinner.PlaySpin();
+            
             Debug.Log($"[{gameObject.name}] calling Harvest()");
-            currentTarget.Harvest(selectedTool);
+            currentTarget.Harvest(selectedTool, spinner);
         }
     }
 
